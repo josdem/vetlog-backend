ba17b47efcaf5a30da072c8a08558aaa76c3b61f
josdem <joseluis.delacruz@gmail.com>
2025-07-22 09:16:08 -0600

    Starting a new Spring Boot backend project :)

---
+++ src/main/java/com/josdem/vetlog/VetlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.josdem.vetlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetlogBackendApplication.class, args);
+	}
+
+}
