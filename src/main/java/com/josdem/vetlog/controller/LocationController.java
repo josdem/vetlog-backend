f1e379d4b7678f1808481af71d720e7674c5a04a
Saveli Gulas <saveli.gulas@gmail.com>
2025-07-27 21:40:42 +0200

    removed unused imports in LocationController

--- src/main/java/com/josdem/vetlog/controller/LocationController.java
+++ src/main/java/com/josdem/vetlog/controller/LocationController.java
@@ -21,14 +21,12 @@ import com.josdem.vetlog.model.Location;
 import com.josdem.vetlog.repository.LocationRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.josdem.vetlog.command.LocationRequestCommand;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @RestController
