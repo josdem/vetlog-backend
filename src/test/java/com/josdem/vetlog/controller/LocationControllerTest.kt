e298142fabf6fe09d7bfcbe4344c71d88286a4d4
Saveli Gulas <saveli.gulas@gmail.com>
2025-07-28 17:08:41 +0200

    changed LocationControllerTest to use @SpringBootTest instead of WebMvcTest

--- src/test/java/com/josdem/vetlog/controller/LocationControllerTest.kt
+++ src/test/java/com/josdem/vetlog/controller/LocationControllerTest.kt
@@ -22,14 +22,16 @@ import com.josdem.vetlog.repository.LocationRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 
-@WebMvcTest(LocationController::class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @TestPropertySource(properties = ["geoToken=testToken"])
 class LocationControllerTest {
 
