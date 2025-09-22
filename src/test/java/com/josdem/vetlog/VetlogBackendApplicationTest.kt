849b9bd8adb68e6d3fffc55af11108d523b024b1
josdem <joseluis.delacruz@gmail.com>
2025-09-22 16:20:33 -0600

    #38 Adding Vetlog backend application test

---
+++ src/test/java/com/josdem/vetlog/VetlogBackendApplicationTest.kt
@@ -0,0 +1,25 @@
+package com.josdem.vetlog
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import org.junit.jupiter.api.assertNotNull
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+
+@SpringBootTest
+internal class VetlogBackendApplicationTest {
+
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `should load application`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+        VetlogBackendApplication.main(arrayOf())
+        assertNotNull(applicationContext, "The application context should have been initialized and not be null")
+    }
+}
